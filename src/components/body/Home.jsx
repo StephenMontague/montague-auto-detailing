a539d20510112cda8e7222ee33891b33535c928c
Stephen Montague <stephen.montague@deposco.com>
2022-02-13 22:20:41 -0500

    Creation of the offers (services) component. Debating adding the services in a hardcoded array within the application or in a database.

--- src/components/body/Home.jsx
+++ src/components/body/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Home = () => {
-	return <div className='home'>Home</div>;
+	return <div className='home'>Hello!</div>;
 };
 
 export default Home;
---
+++ src/components/body/offers/Service.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Service.css';
+import ServiceOffer from './ServiceOffer';
+
+const Services = () => {
+	return (
+		<div className='services'>
+			<ServiceOffer name={'Basic Clean'} />
+		</div>
+	);
+};
+
+export default Services;
