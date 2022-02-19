2c32683f0bb7634d33e1e86ad6a5719dddaba5d5
Stephen Montague <stephen.montague@deposco.com>
2022-02-18 19:33:56 -0500

    Refactoring project to add the backend logic within the overall mad repository.

---
+++ backend/src/main/java/com/montagueautodetailing/website/MontagueAutoDetailingWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.montagueautodetailing.website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MontagueAutoDetailingWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MontagueAutoDetailingWebsiteApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/montagueautodetailing/website/MontagueAutoDetailingWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.montagueautodetailing.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MontagueAutoDetailingWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
