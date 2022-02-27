42ac27c3a88b5293d7e939043916ac0775c7a30a
Stephen Montague <stephen.montague@deposco.com>
2022-02-27 13:09:11 -0500

    Updates to header styling and addition of facebook and instagram links.

--- frontend/src/components/header/SubHeader.jsx
+++ frontend/src/components/header/SubHeader.jsx
@@ -1,22 +1,40 @@
 import React from 'react';
 import './SubHeader.css';
 import { Link } from 'react-router-dom';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 const SubHeader = () => {
 	return (
 		<div className='subheader'>
-			<Link className='link' to='/'>
-				Home
-			</Link>
-			<Link className='link' to='/about-us'>
-				About Us
-			</Link>
-			<Link className='link' to='contact-us'>
-				Contact Us
-			</Link>
-			<Link className='link' to='/services'>
-				Services
-			</Link>
+			<div className='subheader__left'>
+				<Link className='link' to='/'>
+					Home
+				</Link>
+				<Link className='link' to='/about-us'>
+					About Us
+				</Link>
+				<Link className='link' to='contact-us'>
+					Contact Us
+				</Link>
+				<Link className='link' to='/services'>
+					Services
+				</Link>
+			</div>
+			<div className='subheader__right'>
+				<a
+					href='https://www.facebook.com/MontagueAutoDetailing/'
+					target='_blank'
+					rel='noopener noreferrer'>
+					<FacebookIcon />
+				</a>
+				<a
+					href='https://www.instagram.com/montagueautodetailing/'
+					target='_blank'
+					rel='noopener noreferrer'>
+					<InstagramIcon />
+				</a>
+			</div>
 		</div>
 	);
 };
