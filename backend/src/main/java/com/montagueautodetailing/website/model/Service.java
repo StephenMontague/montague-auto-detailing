4be5f953ec91547af47169b5e97a253a068f77f4
Stephen Montague <stephen.d.montague@gmail.com>
2022-03-16 21:41:31 -0400

    Removing .idea from and making some changes to be able to add code to the database on start up of the spring boot application.

--- backend/src/main/java/com/montagueautodetailing/website/model/Service.java
+++ backend/src/main/java/com/montagueautodetailing/website/model/Service.java
@@ -1,12 +1,16 @@
 package com.montagueautodetailing.website.model;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import java.util.Objects;
 
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
@@ -31,4 +35,77 @@ public class Service {
 
     @Column(name = "sale_price")
     private String salePrice;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getServiceCode() {
+        return serviceCode;
+    }
+
+    public void setServiceCode(String serviceCode) {
+        this.serviceCode = serviceCode;
+    }
+
+    public String getServiceName() {
+        return serviceName;
+    }
+
+    public void setServiceName(String serviceName) {
+        this.serviceName = serviceName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getServiceType() {
+        return serviceType;
+    }
+
+    public void setServiceType(String serviceType) {
+        this.serviceType = serviceType;
+    }
+
+    public String getSalePrice() {
+        return salePrice;
+    }
+
+    public void setSalePrice(String salePrice) {
+        this.salePrice = salePrice;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Service)) return false;
+        Service service = (Service) o;
+        return id == service.id && Objects.equals(serviceCode, service.serviceCode) && Objects.equals(serviceName, service.serviceName) && Objects.equals(description, service.description) && Objects.equals(serviceType, service.serviceType) && Objects.equals(salePrice, service.salePrice);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, serviceCode, serviceName, description, serviceType, salePrice);
+    }
+
+    @Override
+    public String toString() {
+        return "Service{" +
+                "id=" + id +
+                ", serviceCode='" + serviceCode + '\'' +
+                ", serviceName='" + serviceName + '\'' +
+                ", description='" + description + '\'' +
+                ", serviceType='" + serviceType + '\'' +
+                ", salePrice='" + salePrice + '\'' +
+                '}';
+    }
 }
