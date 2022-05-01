2651164df189eecc4b634c0103a8be3f8208d895
Stephen Montague <Stephen.D.Montague@gmail.com>
2022-05-01 14:25:56 -0400

    Updated service list to only show certain offers based on the service state

--- frontend/src/components/body/offers/ServiceList.jsx
+++ frontend/src/components/body/offers/ServiceList.jsx
@@ -1,22 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import './ServiceList.css';
 import Service from './Service';
 import OffersService from '../../../services/OffersService';
 import Button from '../../UI/Button';
 
 const ServiceList = () => {
 	const [offers, setOffers] = useState([]);
+	const [service, setService] = useState('Exterior');
 
-	const offersList = offers.map(offer => (
-		<Service
-			key={offer.id}
-			code={offer.serviceCode}
-			name={offer.serviceName}
-			description={offer.description}
-			type={offer.serviceType}
-			price={offer.salePrice}
-		/>
-	));
+	const onServiceTypeHandler = e => {
+		setService(e.target.firstChild.nodeValue);
+	};
 
 	useEffect(() => {
 		getAllOffers();
@@ -26,7 +19,6 @@ const ServiceList = () => {
 		OffersService.getAllServices()
 			.then(response => {
 				setOffers(response.data);
-				console.log(response.data);
 			})
 			.catch(err => {
 				console.log(err);
@@ -34,12 +26,32 @@ const ServiceList = () => {
 	};
 
 	return (
-		<div className='services'>
-			<Button name='Exterior' />
-			<Button name='Interior' />
-			<Button name='Combos' />
-			<Button name='Add Ons' />
-			<ul>{offersList}</ul>
+		<div>
+			<div className='flex justify-center space-x-4'>
+				<Button name='Exterior' onClick={onServiceTypeHandler} />
+				<Button name='Interior' onClick={onServiceTypeHandler} />
+				<Button name='Combo' onClick={onServiceTypeHandler} />
+				<Button name='Add On' onClick={onServiceTypeHandler} />
+			</div>
+			<ul>
+				{offers.map(offer => {
+					if (offer.serviceType === service) {
+						return (
+							<li key={offer.id}>
+								<Service
+									code={offer.serviceCode}
+									name={offer.serviceName}
+									description={offer.description}
+									type={offer.serviceType}
+									price={offer.salePrice}
+								/>
+							</li>
+						);
+					} else {
+						return '';
+					}
+				})}
+			</ul>
 		</div>
 	);
 };
